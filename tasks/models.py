75695ff21d0bf86ac086e9febd2baf7cdb8bb971
Phibeon <phibeon@yahoo.com>
2023-12-15 17:48:55 +0200

    model definition task list vuew

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('todo', 'To Do'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Done'),
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
+    due_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class TaskListView(View):
+    def get(self, request):
+        tasks = Task.objects.all()
+        return render(request, 'task_list.html', {'tasks': tasks})
+
+
