7d14b96a3b6ec0f26a3891087d3ed19bed253867
Phibeon <phibeon@yahoo.com>
2023-12-16 02:34:29 +0200

    addidng a task tas list

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskListView, TaskCreateView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task-list'),
+    path('create/', TaskCreateView.as_view(), name='task-create'), 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 class TaskListView(View):
     def get(self, request):
         tasks = Task.objects.all()
         return render(request, 'task_list.html', {'tasks': tasks})
+    
+
+class TaskCreateView(View):
+    def get(self, request):
+        form = TaskForm()
+        return render(request, 'task_form.html', {'form':form})
+    
+    def post(self, request):
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task-list')
+        return render(request, 'task_form.html', {'form':form})
 
 
